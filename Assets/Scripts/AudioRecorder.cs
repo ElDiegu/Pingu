51c7212abe2508bae57d9395eebeea3c9fbdd22a
luciamol <l.molinero.2019@alumnos.urjc.es>
2023-01-02 21:05:02 +0100

    micro y tienda

---
+++ Assets/Scripts/AudioRecorder.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class AudioRecorder : MonoBehaviour
+{
+    AudioClip recording;
+    AudioSource audioSource;
+    private float startRecordingTime;
+
+    //Get the audiosource here to save resources
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void StartRecording()
+    {
+        //Get the max frequency of a microphone, if it's less than 44100 record at the max frequency, else record at 44100
+        int minFreq;
+        int maxFreq;
+        int freq = 44100;
+        Microphone.GetDeviceCaps("", out minFreq, out maxFreq);
+        if (maxFreq < 44100)
+            freq = maxFreq;
+
+        //Start the recording, the length of 300 gives it a cap of 5 minutes
+        recording = Microphone.Start("", false, 300, 44100);
+        startRecordingTime = Time.time;
+    }
+    public void StopRecording()
+    {
+        //End the recording when the button says
+        Microphone.End("");
+
+        //Trim the audioclip by the length of the recording
+        AudioClip recordingNew = AudioClip.Create(recording.name, (int)((Time.time - startRecordingTime) * recording.frequency), recording.channels, recording.frequency, false);
+        float[] data = new float[(int)((Time.time - startRecordingTime) * recording.frequency)];
+        recording.GetData(data, 0);
+        recordingNew.SetData(data, 0);
+        this.recording = recordingNew;
+
+        //Play recording
+        audioSource.clip = recording;
+        audioSource.Play();
+
+    }
+}
